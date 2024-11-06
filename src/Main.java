625ec3eaa9e8332db655b262b46d85d511784ecb
Yousef Almesaeed <yousefalm@Outlook.com>
2024-11-06 11:26:38 +0300

    Finish task

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner Scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a year");
+        int year = Scanner.nextInt();
+
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("not Leap Year");
+        }
+
+    }
+
 }
